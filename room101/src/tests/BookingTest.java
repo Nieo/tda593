eb4207d481cb509f7e9f75ff7c041c335b213cef
Matz Larsson <matzlarsson94@gmail.com>
2016-01-04 11:41:19 +0100

    updated booking test

--- room101/src/tests/BookingTest.java
+++ room101/src/tests/BookingTest.java
@@ -2,12 +2,12 @@ package tests;
 
 import java.util.Date;
 
+import org.eclipse.emf.common.util.EList;
 import org.junit.Test;
 
 import RootElement.Booking;
-import RootElement.Clerk;
-import RootElement.Guest;
 import RootElement.HotelSystem;
+import RootElement.MakeBooking;
 import RootElement.RoomType;
 import RootElement.impl.HotelFactory;
 
@@ -36,48 +36,47 @@ public class BookingTest {
 	@Test
 	public void testBooking(){
 		HotelSystem hs = HotelFactory.createHotelSystem();
-		checkForClerk(hs);
-		checkForGuest(hs);
+		//Test clerk
+		doTest(hs.getClerk("AwesomeClerk"));
+		//Test guest
+		doTest(hs.getGuest());
 	}
 	
-	private void checkForClerk(HotelSystem hs){
-		Clerk clerk = hs.getClerk("AwesomeClerk");
-		Booking booking = clerk.createBooking();
-		Booking booking2 = clerk.createBooking();
-		int foundRoomsOtherDate = clerk.getAvailableRooms(date2, date3, 1, 0).size();
+	private void doTest(MakeBooking actor){
+		Booking booking = actor.createBooking();
+		Booking booking2 = actor.createBooking();
+		int foundRoomsOtherDate = actor.getAvailableRooms(date2, date3, 1, 0).size();
 		int foundRooms = 0;
-		for(RoomType rt : clerk.getAvailableRooms(date1, date2, 1, 0)){
-			assert(clerk.addRoom(booking, rt, 1, 0));
-			assert(clerk.addRoom(booking2, rt, 1, 0));
+		for(RoomType rt : actor.getAvailableRooms(date1, date2, 1, 0)){
+			assert(actor.addRoom(booking, rt, 1, 0, date1, date2));
+			assert(actor.addRoom(booking2, rt, 1, 0, date1, date2));
 			foundRooms++;
 		}
-		assert(clerk.confirmBooking(booking, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
-		assert(!clerk.confirmBooking(booking2, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
-		assert(!clerk.confirmBooking(booking, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
-		assert(clerk.getAvailableRooms(date1, date2, 1, 0).size() == 0);
-		assert(clerk.getAvailableRooms(date2, date3, 1, 0).size() == foundRoomsOtherDate);
-		assert(clerk.cancelBooking(booking));
-		assert(clerk.getAvailableRooms(date1, date2, 1, 0).size() == foundRooms);
-	}
-	
-	private void checkForGuest(HotelSystem hs){
-		Guest guest = hs.getGuest();
-		Booking booking = guest.createBooking();
-		Booking booking2 = guest.createBooking();
-		int foundRoomsOtherDate = guest.getAvailableRooms(date2, date3, 1, 0).size();
-		int foundRooms = 0;
-		for(RoomType rt : guest.getAvailableRooms(date1, date2, 1, 0)){
-			assert(guest.addRoom(booking, rt, 1, 0));
-			assert(guest.addRoom(booking2, rt, 1, 0));
+		assert(actor.confirmBooking(booking, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
+		assert(!actor.confirmBooking(booking2, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
+		assert(!actor.confirmBooking(booking, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
+		assert(actor.getAvailableRooms(date1, date2, 1, 0).size() == 0);
+		assert(actor.getAvailableRooms(date2, date3, 1, 0).size() == foundRoomsOtherDate);
+		assert(actor.cancelBooking(booking));
+		assert(actor.getAvailableRooms(date1, date2, 1, 0).size() == foundRooms);
+		
+		//Check passportNumber & nextDestination is required
+		Booking booking3 = actor.createBooking();
+		for(RoomType rt : actor.getAvailableRooms(date1, date2, 1, 0)){
+			assert(actor.addRoom(booking3, rt, 1, 0, date1, date2));
 			foundRooms++;
 		}
-		assert(guest.confirmBooking(booking, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
-		assert(!guest.confirmBooking(booking2, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
-		assert(!guest.confirmBooking(booking, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
-		assert(guest.getAvailableRooms(date1, date2, 1, 0).size() == 0);
-		assert(guest.getAvailableRooms(date2, date3, 1, 0).size() == foundRoomsOtherDate);
-		assert(guest.cancelBooking(booking));
-		assert(guest.getAvailableRooms(date1, date2, 1, 0).size() == foundRooms);
-	}
-	
+		assert(!actor.confirmBooking(booking, "Nano", "000-000000", "mail@me.se", "France", -1, null));
+		actor.cancelBooking(booking3);
+		
+		//Check no multiple uses of one and the same room
+		Booking booking4 = actor.createBooking();
+		EList<RoomType> availableRooms = actor.getAvailableRooms(date1, date2, 1, 0);
+		assert(availableRooms.size()>0);
+		RoomType rt = availableRooms.get(0);
+		for(int i = 0; i<availableRooms.size()+1; i++){
+			actor.addRoom(booking4, rt, 1, 0, date1, date2);
+		}
+		assert(!actor.confirmBooking(booking, "Nano", "000-000000", "mail@me.se", "Sweden", -1, null));
+	}	
 }
